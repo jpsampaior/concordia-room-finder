aa93d6b886298f331a3c2ec48798afd92689b17d
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2024-11-09 23:07:22 -0500

    :lipstick: design fix home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="h-screen flex flex-col justify-center text-center space-y-6 lg:text-start">
       <div className="space-y-2">
-        <h1 className="text-2xl text-tertiary font-semibold lg:text-6xl lg:font-extrabold">
+        <h1 className="text-3xl text-tertiary font-semibold lg:text-6xl lg:font-extrabold">
           Buzzing your way <br /> to every room at Concordia!
         </h1>
         <p className="text-zinc-300">
@@ -15,11 +15,11 @@ export default function Home() {
           follow the buzz to easily locate any room on campus.
         </p>
       </div>
-      <div className="space-y-3">
+      <div className="flex flex-col gap-3 lg:flex-row lg:items-center">
         <Input className="w-fit mx-auto lg:m-0" placeholder="Enter room number">
           <Search size={24} className="text-slate-500" />
         </Input>
-        <Button>
+        <Button className="mx-auto lg:m-0" size="lg">
           Find room
           <ArrowRight />
         </Button>
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,12 +5,12 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 dark:focus-visible:ring-zinc-300",
+  "w-fit inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 dark:focus-visible:ring-zinc-300",
   {
     variants: {
       variant: {
         default:
-          "bg-primary text-zinc-50 shadow hover:bg-zinc-900/90 dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-50/90",
+          "bg-primary text-zinc-50 shadow hover:bg-primary/90 dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-50/90",
         destructive:
           "bg-red-500 text-zinc-50 shadow-sm hover:bg-red-500/90 dark:bg-red-900 dark:text-zinc-50 dark:hover:bg-red-900/90",
         outline:
@@ -23,7 +23,7 @@ const buttonVariants = cva(
       size: {
         default: "h-9 px-4 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
+        lg: "h-11 rounded-md px-8",
         icon: "h-9 w-9",
       },
     },
