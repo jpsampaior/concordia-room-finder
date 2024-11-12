27c82985b0f16341f5c3393550025cf9f19c050e
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2024-11-12 10:37:46 -0500

    :sparkles: add select input

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,30 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { roomCoordinates } from "@/lib/coordinates";
 import { ArrowRight, Search } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
 
 export default function Home() {
   const [roomNumber, setRoomNumber] = useState<string>("");
+  const [selectedRoom, setSelectedRoom] = useState<string>("");
 
   function handleRoomNumberChange(event: React.ChangeEvent<HTMLInputElement>) {
     setRoomNumber(event.target.value);
+  }
+
+  function handleSelectChange(value: string) {
+    setSelectedRoom(value);
   }
 
   return (
@@ -25,21 +40,58 @@ export default function Home() {
           follow the buzz to easily locate any room on campus.
         </p>
       </div>
-      <div className="flex flex-col gap-3 lg:flex-row lg:items-center">
-        <Input
-          className="w-fit mx-auto lg:m-0"
-          placeholder="Enter room number"
-          value={roomNumber}
-          onChange={handleRoomNumberChange}
-        >
-          <Search size={24} className="text-slate-500" />
-        </Input>
-        <Link href={`/instructions/${roomNumber.toLowerCase()}`}>
-          <Button className="mx-auto lg:m-0" size="lg">
-            Find room
-            <ArrowRight />
-          </Button>
-        </Link>
+      <div>
+        <h2 className="text-lg text-secondary font-semibold lg:text-2xl lg:font-bold">
+          Option 1: Enter the room code
+        </h2>
+        <div className="flex flex-col gap-3 lg:flex-row lg:items-center">
+          <div>
+            <Input
+              className="w-fit mx-auto lg:m-0"
+              placeholder="Enter room number"
+              value={roomNumber}
+              onChange={handleRoomNumberChange}
+            >
+              <Search size={24} className="text-slate-500" />
+            </Input>
+          </div>
+          <Link href={`/instructions/${roomNumber.toLowerCase()}`}>
+            <Button className="mx-auto lg:m-0" size="lg">
+              Find room
+              <ArrowRight />
+            </Button>
+          </Link>
+        </div>
+      </div>
+      <div>
+        <h2 className="text-lg text-secondary font-semibold lg:text-2xl lg:font-bold">
+          Option 2: Use the select field to find your room
+        </h2>
+        <div className="flex flex-col gap-3 lg:flex-row lg:items-center">
+          <div className="text-zinc-300">
+            <Select onValueChange={handleSelectChange}>
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Select a room" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Webster Library Rooms</SelectLabel>
+                  {Object.keys(roomCoordinates).map((room) => (
+                    <SelectItem key={room} value={room}>
+                      {room.toUpperCase()}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+          <Link href={`/instructions/${selectedRoom.toLowerCase()}`}>
+            <Button className="mx-auto lg:m-0" size="lg">
+              Find room
+              <ArrowRight />
+            </Button>
+          </Link>
+        </div>
       </div>
     </div>
   );
