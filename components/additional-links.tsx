339b77336668295f76736753b09bf7108c8b2c3e
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2024-11-28 11:32:06 -0500

    🎨  improve indentation and component structure

--- components/additional-links.tsx
+++ components/additional-links.tsx
@@ -2,24 +2,11 @@ import Link from "next/link";
 
 export function AdditionalLinks({
   link,
-  title,
   description,
 }: {
   link: string;
-  title: string;
   description: string;
-}) 
-//{
-//   return (
-//     <Link href={link} target="_blank" className="text-zinc-300 hover:underline ease-in-out">
-//       <span className="text-md font-semibold text-primary">{title}: </span>
-//       <span className="text-sm">{description}</span>
-//     </Link>
-//   );
-// 
-//}
-
-{
+}) {
   return (
     <Link
       href={link}
@@ -27,9 +14,7 @@ export function AdditionalLinks({
       rel="noopener noreferrer"
       className="block text-secondary hover:text-primary hover:underline ease-in-out mb-2"
     >
-      <span className="text-md font-semibold text-primary">{title}: </span>
       <span className="text-sm">{description}</span>
     </Link>
   );
 }
-
--- lib/additional-links.ts
+++ lib/additional-links.ts
@@ -1,75 +1,74 @@
 export const resources = [
-    {
-      section: "Food and Dining",
-      items: [
-          {
-      description: "Food & Dining on Campus",
-      link: "https://www.concordia.ca/campus-life/food-dining.html",
-    },
-],
-    },
-    {
-      section: "Study Spaces",    
-      items: [
-          {
-      description: "Webster Library: Quiet study zones, group rooms, and tech-enabled workspaces for students.",
-      link: "https://library.concordia.ca/locations/study-spaces.php",
-    },
-    {
-      description: "Study Areas outside library: Multiple floors with study corners equipped with seating and power outlets.",
-      link: "https://www.concordia.ca/students/success/superb-study-spots-at-concordia.html",
-    },
-    {
-      description: "EV Building Lounges: Open lounges for group and individual study.",
-      link: "https://www.concordia.ca/maps/buildings/concordia-panoramas/ev-lounge-and-terrace.html",
-    },
-
-],
-    },
-    {
-      section: "Campus Safety Services",
-      items: [
-    
-{
-      description: "Emergency Exits.",
-      link: "https://www.concordia.ca/campus-life/safety.html",
-    },
-    {
-      description: "Guidelines for safety during emergencies.",
-      link: "https://www.concordia.ca/campus-life/security.html",
-    },
+  {
+    section: "Food and Dining",
+    items: [
+      {
+        description: "Food & Dining on Campus",
+        link: "https://www.concordia.ca/campus-life/food-dining.html",
+      },
     ],
   },
-{
+  {
+    section: "Study Spaces",
+    items: [
+      {
+        description:
+          "Webster Library: Quiet study zones, group rooms, and tech-enabled workspaces for students.",
+        link: "https://library.concordia.ca/locations/study-spaces.php",
+      },
+      {
+        description:
+          "Study Areas outside library: Multiple floors with study corners equipped with seating and power outlets.",
+        link: "https://www.concordia.ca/students/success/superb-study-spots-at-concordia.html",
+      },
+      {
+        description:
+          "EV Building Lounges: Open lounges for group and individual study.",
+        link: "https://www.concordia.ca/maps/buildings/concordia-panoramas/ev-lounge-and-terrace.html",
+      },
+    ],
+  },
+  {
+    section: "Campus Safety Services",
+    items: [
+      {
+        description: "Emergency Exits.",
+        link: "https://www.concordia.ca/campus-life/safety.html",
+      },
+      {
+        description: "Guidelines for safety during emergencies.",
+        link: "https://www.concordia.ca/campus-life/security.html",
+      },
+    ],
+  },
+  {
     section: "Print Store",
     items: [
-    {
-      description: "DPrint Services on campus.",
-      link: "https://www.concordia.ca/it/services/dprint-public.html",
-    },
-    {
-      description: "Library Printing Services. ",
-      link: "https://www.concordia.ca/print/store.html",
-    },
- ],
-  },
-    {
-      section: "Labs and Facilities",
-      items: [
       {
-      description: "Computer Labs in Hall Building with login credentials.",
-      link: "https://www.concordia.ca/it/services/comp-labs-encs.html",
-    },
-    {
-      description: "Specialized Labs in EV Building",
-      link: "https://www.concordia.ca/it/services/computer-labs.html",
-    },
-    {
-      description: "Library Rooms and facilities.",
-      link: "https://library.concordia.ca/locations/floor-plans/webster-library.php",
-    },
- ],
-},
-  ];
-  
-  
+        description: "DPrint Services on campus.",
+        link: "https://www.concordia.ca/it/services/dprint-public.html",
+      },
+      {
+        description: "Library Printing Services. ",
+        link: "https://www.concordia.ca/print/store.html",
+      },
+    ],
+  },
+  {
+    section: "Labs and Facilities",
+    items: [
+      {
+        description: "Computer Labs in Hall Building with login credentials.",
+        link: "https://www.concordia.ca/it/services/comp-labs-encs.html",
+      },
+      {
+        description: "Specialized Labs in EV Building",
+        link: "https://www.concordia.ca/it/services/computer-labs.html",
+      },
+      {
+        description: "Library Rooms and facilities.",
+        link: "https://library.concordia.ca/locations/floor-plans/webster-library.php",
+      },
+    ],
+  },
+];
